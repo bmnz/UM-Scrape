b0aa9142e6cbc4d73c0944b712bedd76abafdee8
bmnz <ktmannin@gmail.com>
2017-10-07 00:43:54 -0500

    v1.0
    um.db: Updated with all songs through 9-23-2017 show.
    um-allthings.py: Now pulls the allthings webpage from the current year and updates the database, only adding records that weren't in there before.
    
    Todo: dbid is currently an INT that tacks on the latest number... check for overflow conditions.

--- um-allthings.py
+++ um-allthings.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import sqlite3
 import urllib2
+import datetime
 
 def main():
     """Main function to read through data and run some debug stuff from the interactive python shell."""
@@ -46,21 +47,23 @@ def get_soup_show_songs(show):
     return show_songs
 
 def db_populate_songs_table(soup,filename):
-    conn = sqlite3.connect('um.db')
+    conn = sqlite3.connect(file_db)
     dbcurs = conn.cursor()
     #dbcurs.execute("CREATE TABLE songs(dbid INT, song TEXT, show TEXT, track INT)")
     #Need to come up with a new dbid... by querying for the largest existing?
+    #SQL should have an autoincrement function... what you REALLY need is a unique key
     dbcurs.execute("SELECT MAX(dbid) FROM songs")
     foo = dbcurs.fetchone()
     dbid = foo[0]
-    for show in allthings_soup.find_all(class_='setlist'):
+    for show in soup.find_all(class_='setlist'):
         #fill variables that are same for shows, then move on to songs
         show_name = get_soup_show_name(show)
         track = 0
         for song in get_soup_show_songs(show):
             dbid += 1
             track += 1
-            dbcurs.execute("INSERT INTO songs VALUES(?,?,?,?)",(dbid, song, show_name, track)) 
+            #dbcurs.execute("INSERT INTO songs VALUES(?,?,?,?)",(dbid, song, show_name, track)) 
+            dbcurs.execute("INSERT INTO songs (dbid, song, show, track) SELECT ?,?,?,? FROM songs WHERE NOT EXISTS (SELECT * FROM songs WHERE song=? AND show=? AND track=?) LIMIT 1", (dbid, song, show_name, track, song, show_name, track))
             
     #Cross Fingers
     conn.commit()
@@ -74,15 +77,21 @@ def db_populate_setlist_year(year, db_filename):
     allthings_soup = get_soup_from_url(allthings_setlist_year_url(year))
     db_populate_songs_table(allthings_soup,file_db)
 
+def db_update_setlist_year(year, db_filename):
+    allthings_soup = get_soup_from_url(allthings_setlists_year_url(year))
+    db_populate_songs_table(allthings_soup, db_filename)
+
 
 if __name__ == "__main__":
     #Parse html with beautiful soup
     file_allthings = '2017.html'
     file_db = 'um.db'
+    curryear = str(datetime.datetime.now().year)
     #allthings_soup = get_soup_from_url(http_allthings)
     #for year in range(1998,2017):
     #    db_populate_setlist_year(year, file_db)
     #print(allthings_soup.prettify())
     #db_populate_songs_table(allthings_soup,file_db)
-    allthings_soup = get_soup_from_file(file_allthings)
+    #allthings_soup = get_soup_from_file(file_allthings)
+    db_update_setlist_year(curryear, file_db)
 
